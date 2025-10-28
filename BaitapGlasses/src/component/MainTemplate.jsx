04ffb61295238a5cca4c3a37fc5a6ee75c4bd4e8
diepphuphuocloc24 <diepphuphuocloc24@gmail.com>
2025-10-28 16:47:07 +0700

    Loc update JSX

---
+++ BaitapGlasses/src/component/MainTemplate.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeaderComponent from './1.header/HeaderComponent'
+import ChangeGlasses from './2.body'
+
+
+const MainTemplate = () => {
+    return (
+        <div>
+            <HeaderComponent />
+            <ChangeGlasses />
+        </div>
+    )
+}
+
+export default MainTemplate
