3edc45a915c46fe7f1bdfb1a7054ea5861651094
diepphuphuocloc24 <diepphuphuocloc24@gmail.com>
2025-11-01 01:19:31 +0700

    Loc update JS for State

--- BaitapGlasses/src/component/2.body/index.jsx
+++ BaitapGlasses/src/component/2.body/index.jsx
@@ -1,72 +1,69 @@
-import { useState } from 'react'
+import { useState } from "react";
+import dataGlasses from "./dataGlasses.json";
 
 const ChangeGlasses = () => {
+  // Array nhiều kính
+  const [glassesList] = useState(dataGlasses);
 
-    const [image, selectImg] = useState()
+  // Cần lấy 1 kính ra để đeo
+  const [currentGlasses, setCurrentGlasses] = useState(null);
 
-    // const handleChange1 = () => {
-    //     selectImg("./img/v1.png")
-    // }
+  const handleChangeGlasses = (glasses) => {
+    setCurrentGlasses(glasses);
+  };
 
-    // const handleChange2 = () => {
-    //     selectImg("./img/v2.png")
-    // }
+  const handleResetGlasses = () => {
+    setCurrentGlasses(null);
+  };
 
-    // const handleChange3 = () => {
-    //     selectImg("./img/v3.png")
-    // }
+  const renderListGlasses = () => {
+    return glassesList.map((glasses) => {
+      return (
+        <button
+          key={glasses.id}
+          className="flex items-center justify-center p-2 bg-white rounded-xl shadow-md cursor-pointer hover:scale-110 hover:shadow-xl active:scale-95 transition-all duration-300 w-25 h-20"
+          onClick={() => handleChangeGlasses(glasses)}
+        >
+          <img src={glasses.pic} alt={glasses.name} className="w-16" />
+        </button>
+      );
+    });
+  };
 
-    // const handleChange4 = () => {
-    //     selectImg("./img/v4.png")
-    // }
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 py-10">
+      <img src="./img/model.jpg" alt="model" className="w-[300px] relative" />
 
-    // const handleChangeDefault = () => {
-    //     selectImg()
-    // }
+      {currentGlasses && (
+        <img
+          src={currentGlasses.url}
+          alt={currentGlasses.name}
+          className="w-[180px] absolute top-61 opacity-80"
+        />
+      )}
 
-    const handleChangeImage = (pic) => {
-        selectImg(pic)
-    }
-
-    return (
-        <div className="flex flex-col items-center justify-center gap-6 py-10">
-            <img src="./img/model.jpg" alt="model" className="w-[300px]" />
-
-            <img src={image} className="w-[180px] absolute top-62 opacity-80" />
-
-            <div className="bg-blue-100 backdrop-blur-md rounded-2xl shadow-lg p-6 w-fit mx-auto flex flex-wrap justify-center gap-5 border border-gray-200">
-                <button
-                    className="flex items-center justify-center p-2 bg-white rounded-xl shadow-md cursor-pointer hover:scale-110 hover:shadow-xl active:scale-95 transition-all duration-300 w-25 h-20" onClick={() => handleChangeImage("./img/v1.png")}
-                >
-                    <img src="./img/g1.jpg" alt="glasses-1" className="w-16" />
-                </button>
-
-                <button
-                    className="flex items-center justify-center p-2 bg-white rounded-xl shadow-md cursor-pointer hover:scale-110 hover:shadow-xl active:scale-95 transition-all duration-300 w-25 h-20" onClick={() => handleChangeImage("./img/v2.png")}
-                >
-                    <img src="./img/g2.jpg" alt="glasses-2" className="w-16" />
-                </button>
-
-                <button
-                    className="flex items-center justify-center p-2 bg-white rounded-xl shadow-md cursor-pointer hover:scale-110 hover:shadow-xl active:scale-95 transition-all duration-300 w-25 h-20" onClick={() => handleChangeImage("./img/v3.png")}
-                >
-                    <img src="./img/g3.jpg" alt="glasses-3" className="w-16" />
-                </button>
-
-                <button
-                    className="flex items-center justify-center p-2 bg-white rounded-xl shadow-md cursor-pointer hover:scale-110 hover:shadow-xl active:scale-95 transition-all duration-300 w-25 h-20" onClick={() => handleChangeImage("./img/v4.png")}
-                >
-                    <img src="./img/g4.jpg" alt="glasses-4" className="w-16" />
-                </button>
-
-                <button
-                    className="flex items-center justify-center p-2 bg-white rounded-xl shadow-md cursor-pointer hover:scale-110 hover:shadow-xl active:scale-95 transition-all duration-300 w-25 h-20" onClick={() => handleChangeImage()}
-                >
-                    <i class="fa-solid fa-circle-xmark text-rose-600 text-3xl"></i>
-                </button>
-            </div>
+      {currentGlasses && (
+        <div className="absolute bottom-23 bg-orange-400 opacity-70 p-1 w-[300px]">
+          <h2 className="text-indigo-700 text-2xl font-bold mb-2">
+            {currentGlasses.name}
+          </h2>
+          <p className="text-white text-md mb-1">{currentGlasses.desc}</p>
         </div>
-    )
-}
+      )}
+
+      <div className="bg-blue-100 backdrop-blur-md rounded-2xl shadow-lg p-6 w-fit mx-auto flex flex-wrap justify-center gap-5 border border-gray-200">
+        {/* Gọi hàm */}
+        {renderListGlasses()}
+
+        <button
+          className="flex items-center justify-center p-2 bg-red-400 text-white font-semibold rounded-xl shadow-md hover:bg-red-500 hover:scale-105 transition-all duration-300 w-25 h-20"
+          onClick={handleResetGlasses}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default ChangeGlasses;
