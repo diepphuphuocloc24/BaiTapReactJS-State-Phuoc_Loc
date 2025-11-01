2658335ef10c6f946430e830f4a5cac976442752
diepphuphuocloc24 <diepphuphuocloc24@gmail.com>
2025-11-01 15:32:29 +0700

    Loc update Props for homework

--- BaitapGlasses/src/component/2.body/index.jsx
+++ BaitapGlasses/src/component/2.body/index.jsx
@@ -1,69 +1,54 @@
 import { useState } from "react";
 import dataGlasses from "./dataGlasses.json";
+import Glasses from "./Glasses";
+import ShowUp from "./ShowUp";
 
 const ChangeGlasses = () => {
-  // Array nhiều kính
-  const [glassesList] = useState(dataGlasses);
+  const [listGlasses] = useState(dataGlasses)
 
-  // Cần lấy 1 kính ra để đeo
-  const [currentGlasses, setCurrentGlasses] = useState(null);
+  // Lấy 1 loại kính
+  const [currentGlasses, setCurrentGlasses] = useState(null)
 
-  const handleChangeGlasses = (glasses) => {
-    setCurrentGlasses(glasses);
-  };
+  // Lấy dữ liệu từ con sang cha
+  const getInfoFromGlasses = (selectedGlasses) => {
+    setCurrentGlasses(selectedGlasses)
+  }
 
-  const handleResetGlasses = () => {
-    setCurrentGlasses(null);
-  };
-
-  const renderListGlasses = () => {
-    return glassesList.map((glasses) => {
+  // Render tất cả kính ra UI
+  const renderGlassesUI = () => {
+    return listGlasses.map((glasses) => {
       return (
-        <button
-          key={glasses.id}
-          className="flex items-center justify-center p-2 bg-white rounded-xl shadow-md cursor-pointer hover:scale-110 hover:shadow-xl active:scale-95 transition-all duration-300 w-25 h-20"
-          onClick={() => handleChangeGlasses(glasses)}
-        >
-          <img src={glasses.pic} alt={glasses.name} className="w-16" />
-        </button>
-      );
-    });
-  };
+        <Glasses key={glasses.id} glassesProp={glasses} onSelectedGlasses={getInfoFromGlasses} />
+      )
+    })
+  }
+
+  // Khi bấm nút Reset
+  const handleResetGlasses = () => {
+    setCurrentGlasses(null)
+  }
 
   return (
     <div className="flex flex-col items-center justify-center gap-6 py-10">
       <img src="./img/model.jpg" alt="model" className="w-[300px] relative" />
 
       {currentGlasses && (
-        <img
-          src={currentGlasses.url}
-          alt={currentGlasses.name}
-          className="w-[180px] absolute top-61 opacity-80"
-        />
-      )}
-
-      {currentGlasses && (
-        <div className="absolute bottom-23 bg-orange-400 opacity-70 p-1 w-[300px]">
-          <h2 className="text-indigo-700 text-2xl font-bold mb-2">
-            {currentGlasses.name}
-          </h2>
-          <p className="text-white text-md mb-1">{currentGlasses.desc}</p>
-        </div>
+        <ShowUp key={currentGlasses.id} glassesProp={currentGlasses} />
       )}
 
       <div className="bg-blue-100 backdrop-blur-md rounded-2xl shadow-lg p-6 w-fit mx-auto flex flex-wrap justify-center gap-5 border border-gray-200">
-        {/* Gọi hàm */}
-        {renderListGlasses()}
+        {/* Gọi hàm vì mới vào là show hết */}
+        {renderGlassesUI()}
 
+        {/* Thêm nút Reset để trả về không có kính, giúp cho UI tốt hơn thay vì phải F5 */}
         <button
           className="flex items-center justify-center p-2 bg-red-400 text-white font-semibold rounded-xl shadow-md hover:bg-red-500 hover:scale-105 transition-all duration-300 w-25 h-20"
-          onClick={handleResetGlasses}
-        >
+          onClick={() => handleResetGlasses()}>
           Reset
         </button>
       </div>
-    </div>
-  );
-};
+    </div >
+  )
+}
 
 export default ChangeGlasses;
